748b2ab081f883cc445d718b506ae88549d5af86
Anton <heni.kurniyanto@kuncie.com>
2022-08-27 23:38:57 +0700

    fix(clean): remove unused code

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
-// import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_bloc_app/presentation/router/app_router.dart';
-import 'package:flutter_bloc_app/presentation/screens/first_screen/first_screen.dart';
 
 import 'business_logic/cubits/counters/counter_cubit.dart';
 
@@ -25,12 +23,6 @@ class BlocApp extends StatelessWidget {
           primarySwatch: Colors.blue,
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
-        // home: const FirstScreen(color: Colors.yellowAccent, title: 'Home',),
-        // initialRoute: '/firstScreen',
-        // onGenerateRoute: (settings) {
-        //   _appRouter.OnGenerateRoute(settings);
-        //   return;
-        // },
         onGenerateRoute: (settings) => _appRouter.OnGenerateRoute(settings),
       ),
     );
