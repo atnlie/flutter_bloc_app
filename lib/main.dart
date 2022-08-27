c68d6139c26cad9d0c6139e86c25464d755dce71
Anton <heni.kurniyanto@kuncie.com>
2022-08-27 15:33:35 +0700

    fix(architecture): update architecture

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_bloc_app/cubits/counters/counter_cubit.dart';
+
+import 'business_logic/cubites/counters/counter_cubit.dart';
+
 
 void main() {
   runApp(const MyApp());
